d8a290a2edb1e68bc1efe7c87a4a42374c48d8a7
Jeremy Long <jeremy.long@gmail.com>
2024-10-13 07:03:51 -0400

    fix: attempt 7 to debug/stabilize the ant integration tests

--- ant/src/test/java/org/owasp/dependencycheck/taskdefs/DependencyCheckTaskIT.java
+++ ant/src/test/java/org/owasp/dependencycheck/taskdefs/DependencyCheckTaskIT.java
@@ -69,6 +69,7 @@ public class DependencyCheckTaskIT extends BaseDBTestCase {
             LOGGER.error("\n\n" + buildFileRule.getLog());
             LOGGER.error("--------------------------------------");
             throw new Exception("Ant Log:\n\n:" + buildFileRule.getLog(), ex);
+            
         }
         assertTrue("DependencyCheck report was not generated", report.exists());
     }
